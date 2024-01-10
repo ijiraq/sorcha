6991d39241abe6497dd401d8bdebad8eec81f1e2
Jake Kurlander <jkurla@uw.edu>
2024-01-10 12:09:37 -0800

    Read-only and retries to PPReadPointingDatabase

--- src/sorcha/modules/PPReadPointingDatabase.py
+++ src/sorcha/modules/PPReadPointingDatabase.py
@@ -28,17 +28,25 @@ def PPReadPointingDatabase(bsdbname, observing_filters, dbquery, surveyname):
 
     pplogger = logging.getLogger(__name__)
 
-    con = sqlite3.connect(bsdbname)
+    con = sqlite3.connect(f'file:{bsdbname}?mode=ro', uri=True)
 
-    try:
-        df = pd.read_sql_query(dbquery, con)
-    except Exception:
-        pplogger.error(
-            "ERROR: PPReadPointingDatabase: SQL query on pointing database failed. Check that the query is correct in the config file."
-        )
-        sys.exit(
-            "ERROR: PPReadPointingDatabase: SQL query on pointing database failed. Check that the query is correct in the config file."
-        )
+    # For no more than 160 seconds (120 + 40), attempt to read the database.
+    # On failure, wait a random number of seconds between 20 and 40 and try again.
+    # Adds robustness to read failures, which still occur when hundreds of Sorcha
+    # tasks try to read the same pointing database simultaneously even in read-only mode.
+
+    attempt_reading_time = 0
+    while attempt_reading_time < 120:
+        try:
+            df = pd.read_sql_query(dbquery, con)
+            break
+        except Exception as e:
+            pplogger.error(
+                "ERROR: PPReadPointingDatabase: SQL query on pointing database failed. Check that the query is correct in the config file."
+            )
+            delay_time = randint(20, 40)
+            attempt_reading_time += delay_time
+            sleep(delay_time)
 
     df["observationId_"] = df["observationId"]
     df = df.rename(columns={"observationId": "FieldID"})
